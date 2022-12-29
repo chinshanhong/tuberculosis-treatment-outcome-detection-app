3370500de26c4a662539395c7c4f780d49c74f32
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-12-29 23:12:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ with tab1:
                 'Please select your occupation',
                 ['Radiologist', 'General practitioner', 'Other', 'Not Reported']
             )
-    
+
             # Second widget to get the treatment status of TB patient
             treatment_status = st.selectbox("Please select your patient's treatment status",
                                             ['Treatment ended', 'Continuation of Treatment',
@@ -36,7 +36,7 @@ with tab1:
                                                 'Patient stopped treatment', 'Terminated from study',
                                                 'New drugs available', 'Drug(s) no longer available',
                                                 'Adverse event', 'Not Reported'])
-    
+
             # Third widget to select the risk factors that the TB patient currently having
             risk_factors = st.multiselect('Please select all the risk factors that your patient exhibit',
                                             ['Current smoker', 'Documented MDR contact', 'Patient alcohol abuse',
@@ -44,13 +44,13 @@ with tab1:
                                             'Patient drug abuse', 'Immigrants', 'Refugees', 'Internal migrants',
                                             'Not Reported'],
                                             ['Not Reported'])
-    
+
             # Fourth widget to select the pleural effusion percent of hemithorax involved
             pleural_effusion_percent = st.selectbox('Please select the pleural effusion percent of hemithorax',
                                                     ['Less than 50', '0',
                                                         'Greater than or equal to 50',
                                                         'Not Reported'])
-    
+
             # Fifth widget to select the drug regimen that the patient is taking
             drug_regimen = st.multiselect('Please select the drug regimen that your patient is taking',
                                             ['Bedaquiline', 'Clofazimine', 'Cycloserine', 'Levofloxacin',
@@ -62,7 +62,7 @@ with tab1:
                                             'Ofloxacin', 'Amikacin', 'Ethionamide', 'Terizidone',
                                             'Antiretroviral therapy', 'Cotrimoxazol preventive', 'Clarithromycin',
                                             'Fluoroquinolones', 'Not Reported'], ['Not Reported'])
-    
+
         with col2:
             # Sixth widget to select the location of small nodules that the patient is taking
             small_nodules = st.multiselect('Does small nodules exist?',
@@ -72,7 +72,7 @@ with tab1:
                                             'Upper Left Sextant-No', 'Upper Right Sextant-No', 'Lower Right Sextant-No',
                                             'None', 'Not Reported'],
                                             ['Not Reported'])
-    
+
                 # Seventh widget to select the location of small nodules that the patient is taking
             calcified_nodules = st.multiselect('Does calcified nodules exist?',
                                                 ['Lower Left Sextant-Yes', 'Lower Left Sextant-No',
@@ -84,35 +84,35 @@ with tab1:
                                                 'Upper Left Sextant-No', 'Upper Right Sextant-No', 'Lower Right Sextant-No',
                                                 'None', 'Not Reported'],
                                                 ['Not Reported'])
-    
+
                 # Eight widget to select the gene name
             gene_name = st.multiselect('Please select your patient genomic sequence',
                                           ['katG', 'rpoB', 'rpsL', 'embB', 'inhA-Pro',
                                         'gyrA', 'rrs', 'pncA', 'inhA', 'Not Reported'],
                                         ['Not Reported'])
-    
+
                 # Ninth widget to select the result of drug sensitivity test
             hain_rifampicin = st.selectbox('Please select the drug sensitivity test result of rifampicin',
                                             ['Resistant', 'Sensitive', 'Not Reported'])
-    
+
                 # Tenth widget to select the result of drug sensitivity test
             hain_isoniazid = st.selectbox('Please select the drug sensitivity test result of isoniazid',
                                             ['Resistant', 'Sensitive', 'Intermediate', 'Not Reported'])
-    
+
         submitted = st.form_submit_button("Predict")
-    
+
         # Add input data into numpy array
         if submitted:
             social_risk_factor_list = ', '.join(risk_factors)
-    
+
             regimen_drug_list = ', '.join(drug_regimen)
-    
+
             gene_name_list = ', '.join(gene_name)
-    
+
             small_nodules_list = ', '.join(small_nodules)
-    
+
             calcified_nodules_list = ', '.join(calcified_nodules)
-    
+
             input_data = {
                 'treatment_status': [treatment_status],
                 'hain_rifampicin': [hain_rifampicin],
@@ -125,39 +125,39 @@ with tab1:
                 'smallnodules': [small_nodules_list],
                 'isanynoncalcifiednoduleexist': [calcified_nodules_list]
             }
-    
+
             df = pd.DataFrame(input_data)
-    
+
             lr_model = pickle.load(open(
                 'lr_model.pkl',
                 'rb'))
-    
+
             encoder = pickle.load(open('encoder.pkl', 'rb'))
             scaler = pickle.load(open('scaler.pkl', 'rb'))
-    
+
             df = encoder.transform(df)
             df = scaler.transform(df)
-    
+
             result = lr_model.predict(df)
-    
+
             if result == 0:
                 st.session_state.result = 0
                 st.session_state.treatment_outcome = 'Cured'
-    
+
             elif result == 1:
                 st.session_state.result = 1
                 st.session_state.treatment_outcome = 'Died'
-    
+
             elif result == 2:
                 st.session_state.result = 2
                 st.session_state.treatment_outcome = 'Unknown'
-    
-    
-    st.subheader("Detection Result")
+
+
+    st.write("Detection Result")
     image_path = ''
     outcome = ''
     col3, col4 = st.columns(2)
-    
+
     with col3:
         if st.session_state.result == 0:
             image_path = 'cured.png'
@@ -168,7 +168,7 @@ with tab1:
         elif st.session_state.result == 2:
             image_path = 'unknown.png'
             st.image(image_path, width=200)
-    
+
     with col4:
         if st.session_state.result == 0:
             st.header(f':green[{st.session_state.treatment_outcome}]')
