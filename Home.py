b0a45b57cbae15aad41ef4e7759cb84d1cd9eb68
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-12-30 15:35:18 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(
 )
 
 
-st.write("Tuberculosis Treatment Outcomes Detector")
+st.header("Tuberculosis Treatment Outcomes Detector")
 
 st.sidebar.success("Select a demo above.")
 
