b15f2425fd5e685ab8d4c31d25cfe6f7efb5bf20
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-12-30 15:28:42 +0800

    Add files via upload

---
+++ pages/2_Batch_Detection.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+st.set_page_config(
+    page_title='Batch Detection',
+    layout='centered'
+)
+
+st.write('Hello')
