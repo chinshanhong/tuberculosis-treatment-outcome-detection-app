95bd14634b39d5c8fd91099018bf2dabdb67fce4
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-12-30 21:37:23 +0800

    Update 2_Batch_Detection.py

--- pages/2_Batch_Detection.py
+++ pages/2_Batch_Detection.py
@@ -3,11 +3,6 @@ import pandas as pd
 import numpy as np
 import pickle
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pickle
-
 st.set_page_config(
     page_title='Tuberculosis Treatment Outcomes Detector',
     layout='centered'
