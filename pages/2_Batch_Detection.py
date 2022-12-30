8bf339fa8d188ba8b7592cf7a23af8642ec7a0fb
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-12-30 16:04:42 +0800

    Update 2_Batch_Detection.py

--- pages/2_Batch_Detection.py
+++ pages/2_Batch_Detection.py
@@ -10,7 +10,15 @@ st.set_page_config(
 
 st.title('Batch Detection')
 
+def predict(input_data):
+    st.write("Hello")
+
 csv_file = st.file_uploader("Choose a CSV file", type='csv')
+input_data = None;
+
 if csv_file is not None:
     input_data = pd.read_csv(csv_file)
-    st.write(input_data)
+
+if st.button('Predict'):
+    predict(input_data)
+
