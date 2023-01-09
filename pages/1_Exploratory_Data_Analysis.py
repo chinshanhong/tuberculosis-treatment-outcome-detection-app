e02c5ffef12a53ef4d7481df9490ca559c2ff60a
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2023-01-09 20:53:27 +0800

    Add files via upload

---
+++ pages/1_Exploratory_Data_Analysis.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+
+st.set_page_config(layout="centered", page_title='TB Outcomes Oracle', page_icon='🔮')
+
+st.title("Exploratory Data Analysis")
+
+st.markdown("""
+    You can upload your TB medical data here and explore it with our tools!
+""")
+
+data = None
+
+def explore(data):
+    pr = data.profile_report()
+    st_profile_report(pr)
+
+
+uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+
+if st.button("Explore"):
+    explore(data)
