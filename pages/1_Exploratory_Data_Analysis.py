b797a7503cd5ecd5712702079a7ea1ce297eeed8
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2023-01-09 21:29:58 +0800

    Update 1_Exploratory_Data_Analysis.py

--- pages/1_Exploratory_Data_Analysis.py
+++ pages/1_Exploratory_Data_Analysis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pandas_profilling
+import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="centered", page_title='TB Outcomes Oracle', page_icon='🔮')
