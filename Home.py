0f7055a0479948dc9e6dc4caee2f0fdeb9b557e1
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2023-01-16 10:59:26 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -48,7 +48,7 @@ st.markdown("""
     | Unknown             | A patient for whom no treatment outcome is assigned. (This includes cases “transferred out” to another treatment unit and whose treatment outcome is unknown). |
     
     #### User Manual
-    To undertstnad how to use the TB Outcomes Oracle in details, please read the [TB Outcomes Oracle User Manual](https://drive.google.com/file/d/13Q-lUBe5uA9yyu1syBOolFjgnlo-OoKq/view?usp=sharing).
+    To understand how to use the TB Outcomes Oracle in details, please read the [TB Outcomes Oracle User Manual](https://drive.google.com/file/d/13Q-lUBe5uA9yyu1syBOolFjgnlo-OoKq/view?usp=sharing).
     
 """
 )
